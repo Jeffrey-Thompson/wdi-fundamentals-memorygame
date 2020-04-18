de5c9766099a2394ba63bc6085f9e3ac8b573e1c
Jeffrey Thompson <jeffathomp@gmail.com>
2020-04-18 16:39:40 -0500

    created game logic

--- memory_game/memory-game/js/main.js
+++ memory_game/memory-game/js/main.js
@@ -1,4 +1,24 @@
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png",
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}];
 let cardsInPlay = [];
 let cardID;
 function checkForMatch() {
@@ -10,8 +30,10 @@ function checkForMatch() {
 	}
 }
 function flipCard(cardID) {
-	cardsInPlay.push(cards[cardID]);
-	console.log("User flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log("User flipped " + cards[cardID].rank);
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
 	checkForMatch();
 }
 flipCard(0);
