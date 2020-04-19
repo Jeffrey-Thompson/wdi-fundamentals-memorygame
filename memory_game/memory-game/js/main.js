6689915367edcc4086cb2ca2e479f0de6038874b
Jeffrey Thompson <jeffathomp@gmail.com>
2020-04-19 17:37:36 -0500

    created game logic

--- memory_game/memory-game/js/main.js
+++ memory_game/memory-game/js/main.js
@@ -22,19 +22,36 @@ cardImage: "images/king-of-diamonds.png"
 let cardsInPlay = [];
 let cardID;
 function checkForMatch() {
+	this.setAttribute('src', cards[cardID].cardImage);
 		if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			console.log("You found a match!");
 		} else
 			console.log("Sorry, try again.")
 	}
-}
-function flipCard(cardID) {
+};
+function flipCard() {
+	let cardID = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardID].rank);
 	console.log("User flipped " + cards[cardID].rank);
 	console.log(cards[cardID].cardImage);
 	console.log(cards[cardID].suit);
 	checkForMatch();
-}
-flipCard(0);
-flipCard(2);
+};
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		//creates cardElement and sets to img type
+		let cardElement = document.createElement('img');
+		//sets cardElement attributes to image sorce 
+		cardElement.setAttribute('src', 'images/back.png');
+		//gives cardElement a id
+		cardElement.setAttribute('data-id', i);
+		//appends cardElement to game-board
+		document.getElementById('game-board')[0].appendChild(cardElement);
+	}
+};
+
+cardElement.addEventListener('click', flipCard());
+
+createBoard();
